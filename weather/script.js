1bce3c8c6cba39bfc8c76fdaa93f3f472a105e85
Artyom <arkrasovski1996@gmail.com>
2021-07-15 19:12:49 +0300

    init: add weather-app

---
+++ weather/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+async function getLinkToImage() {
+  const url =
+    "https://api.unsplash.com/photos/random?query=morning&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17";
+  const res = await fetch(url);
+  const data = await res.json();
+  container.style.cssText = `background-image: url("${data.urls.regular}");`;
+  console.log(data.urls.regular);
+}
+getLinkToImage();
