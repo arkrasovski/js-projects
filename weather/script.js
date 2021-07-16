f7f5561cbe85966d4f0831b06accfeb14d2dafe3
Artyom <arkrasovski1996@gmail.com>
2021-07-16 18:51:43 +0300

    feat: add custom select

--- weather/script.js
+++ weather/script.js
@@ -9,3 +9,37 @@ async function getLinkToImage() {
   console.log(data.urls.regular);
 }
 getLinkToImage();
+
+let select = function () {
+  let selectHeader = document.querySelectorAll(".select__header");
+  let selectItem = document.querySelectorAll(".select__item");
+
+  selectHeader.forEach((item) => {
+    item.addEventListener("click", selectToggle);
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", selectChoose);
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle("is-active");
+    if (this.parentElement.classList.contains("is-active")) {
+      document.querySelector(
+        ".select"
+      ).style.cssText = `border-radius: 5px 5px 0 0;`;
+    } else {
+      document.querySelector(".select").style.cssText = `border-radius: 5px;`;
+    }
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest(".select"),
+      currentText = select.querySelector(".select__current");
+    currentText.innerText = text;
+    select.classList.remove("is-active");
+  }
+};
+
+select();
